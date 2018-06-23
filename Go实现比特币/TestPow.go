472b696617133c8def50c08df08c9e8b04a0ec93
mudy <2808050775@qq.com>
2018-06-23 23:23:18 +0800

    计时

--- Go实现比特币/TestPow.go
+++ Go实现比特币/TestPow.go
@@ -4,16 +4,19 @@ import (
 	"crypto/sha256"
 	"strconv"
 	"fmt"
+	"time"
 )
 
 func main() {
 
-	for i := 0; i< 1000000; i++ {
+	for i := 0; i< 100000000; i++ {
 		data := sha256.Sum256([]byte(strconv.Itoa(i)))
-
-		res := string(data[len(data)-2:])
-		if  res == "00"{
+		start := time.Now()
+		res := string(data[len(data)-3:])
+		if  res == "000"{
+			timeUsed := time.Since(start) //挖矿用时
 			fmt.Printf("%d %s %s\n\n",i,res,data)
+			fmt.Println(timeUsed)
 		}
 
 	}
